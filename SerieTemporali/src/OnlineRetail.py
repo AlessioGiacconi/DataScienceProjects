ed1d1b8906aba39cb997f3f7c87d7b68b39292f6
Francesco Camplese <falcone6675@gmail.com>
2025-01-09 16:22:25 +0100

    remove records with no CustomerID ans UnitPrice == 0, add TotalPrice feature

--- SerieTemporali/src/OnlineRetail.py
+++ SerieTemporali/src/OnlineRetail.py
@@ -1,24 +1,13 @@
 import pandas as pd
 
-df_csv = pd.read_csv('C:\\Users\\falco\\PycharmProjects\\ProgettoSerieTemporali\\Dataset\\Online_Retail.csv')
+df_csv = pd.read_csv('C:\\Users\\falco\\PycharmProjects\\ProgettoSerieTemporali\\DataScienceProjects\\SerieTemporali\\Dataset\\Online_Retail.csv')
 df_csv.info()
 
-# Controllo preliminare dei dati mancanti
-missing_values = df_csv.isnull().sum()
+df_csv = df_csv.dropna(subset=['CustomerID'])
 
-# Controlliamo eventuali valori unici in alcune colonne chiave per individuare anomalie
-unique_values_summary = {
-    "InvoiceNo": df_csv["InvoiceNo"].nunique(),
-    "StockCode": df_csv["StockCode"].nunique(),
-    "CustomerID": df_csv["CustomerID"].nunique(),
-    "Country": df_csv["Country"].nunique(),
-}
+df_csv.info()
 
-print(missing_values)
+unit_price_not_zero = df_csv[df_csv['UnitPrice'] > 0]
 
-print(unique_values_summary)
-
-stockcode_total_count = df_csv["StockCode"].count()
-print(stockcode_total_count)
-
-unitprice_zero = df_csv[df_csv["UnitPrice"] == 0]
+unit_price_not_zero['TotalPrice'] = (unit_price_not_zero['UnitPrice'] * unit_price_not_zero['Quantity']).round(2)
+df_csv.info()
