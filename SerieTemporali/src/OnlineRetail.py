61608961f18cb5ff7dc5696f4317e228d8016f3b
Francesco Camplese <falcone6675@gmail.com>
2025-01-10 15:52:08 +0100

    Add Date and Time features

--- SerieTemporali/src/OnlineRetail.py
+++ SerieTemporali/src/OnlineRetail.py
@@ -7,7 +7,12 @@ df_csv = df_csv.dropna(subset=['CustomerID'])
 
 df_csv.info()
 
-unit_price_not_zero = df_csv[df_csv['UnitPrice'] > 0]
+unit_price_not_zero = df_csv[df_csv['UnitPrice'] > 0].copy()
 
 unit_price_not_zero['TotalPrice'] = (unit_price_not_zero['UnitPrice'] * unit_price_not_zero['Quantity']).round(2)
-df_csv.info()
+
+unit_price_not_zero['InvoiceDate'] = pd.to_datetime(unit_price_not_zero['InvoiceDate'])
+
+unit_price_not_zero['Date'] = unit_price_not_zero['InvoiceDate'].dt.date
+unit_price_not_zero['Time'] = unit_price_not_zero['InvoiceDate'].dt.time
+unit_price_not_zero.info()
