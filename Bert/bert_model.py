095b65d8ea0f798fa23b756598ffc6c22a9778dc
AlessioGiacconi <s1116262@studenti.univpm.it>
2025-02-04 11:56:09 +0100

    tokenizzazione

--- Bert/bert_model.py
+++ Bert/bert_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
-from sklearn.model_selection import train_test_split
+import torch
+
 
 base_dir = Path(__file__).resolve().parent
 
@@ -9,8 +10,17 @@ file_path = base_dir / 'Dataset' / 'cleaned_tweets.csv'
 df_csv = pd.read_csv(file_path)
 df_csv.info()
 
-# Suddividi il dataset in training set (80%) e test set (20%)
-train_set, test_set = train_test_split(df_csv, test_size=0.2, random_state=42, shuffle=True)
+label_mapping = {
+    "Not Suicide post": 0,
+    "Potential Suicide post ": 1
+}
+
+df_csv['Suicide'] = df_csv['Suicide'].map(label_mapping)
+
+
+# Controllare la disponibilità della GPU
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 
 
 
