f1587b64fa7bf95a1a51b3e61a09b302b959c1c8
Francesco Camplese <falcone6675@gmail.com>
2025-02-13 11:18:38 +0100

    add cerca_film intent

--- ChatBot/actions/actions.py
+++ ChatBot/actions/actions.py
@@ -25,3 +25,29 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+import pandas as pd
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+df = pd.read_csv("dataset\\tmdb_movies.csv")
+
+class ActionCercaFilm(Action):
+    def name(self):
+        return "action_cerca_film"
+
+    def run(self, dispatcher, tracker, domain):
+        titolo = tracker.get_slot("title")
+
+        if titolo:
+            film = df[df["title"].str.lower() == titolo.lower()]
+            if not film.empty:
+                risposta = f"🎬 {film.iloc[0]['title']} è uscito il {film.iloc[0]['release_date']}."
+                dispatcher.utter_message(risposta)
+            else:
+                dispatcher.utter_message("Non ho trovato questo film 😢")
+        else:
+            dispatcher.utter_message("Dimmi il titolo del film che cerchi 🎬")
+
+        return []
+
